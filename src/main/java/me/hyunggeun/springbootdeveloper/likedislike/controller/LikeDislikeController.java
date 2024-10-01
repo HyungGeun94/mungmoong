08a2b45aa106dc76dcc74ad75410e706e8add8a8
cloud <goorm94@naver.com>
2024-10-01 13:40:35 +0900

    좋아요,싫어요 로직 정리

--- src/main/java/me/hyunggeun/springbootdeveloper/likedislike/controller/LikeDislikeController.java
+++ src/main/java/me/hyunggeun/springbootdeveloper/likedislike/controller/LikeDislikeController.java
@@ -10,9 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/article")
@@ -21,41 +18,19 @@ public class LikeDislikeController {
 
     private final LikeDislikeService likeDislikeService;
 
-    // 좋아요 누르기
-
     /**
-     *
-     * @param articleId
-     * @return
-     * 두개 로직 합치기.
-     * 어떻게 합치냐면
-     * html에서 보내는 Pathvariable에 따라 {like}/{dislike}
-     * 일단 보내고 서비스단도 합쳐서 분기처리하기. 그러면 더 깔끔할듯.
      * @PathVariable은 DTO에 담지 못하기 때문에 지금 방법 그대로 유지.
      */
 
-    @PostMapping("/{articleId}/like")
-    public ResponseEntity<LikeDislikeResponseDTO> likePost(@PathVariable Long articleId) {
+    @PostMapping("/{articleId}/{action}")
+    public ResponseEntity<LikeDislikeResponseDTO> likeDislikePost(
+            @PathVariable Long articleId,
+            @PathVariable String action) {
 
-        LikeDislikeResponseDTO likeDislikeResponseDTO = likeDislikeService.likeArticle(articleId);
+        LikeDislikeResponseDTO likeDislikeResponseDTO = likeDislikeService.likeDislikeArticle(articleId,action);
 
 
         return ResponseEntity.ok(likeDislikeResponseDTO);
     }
 
-    // 싫어요 누르기
-    @PostMapping("/{articleId}/dislike")
-    public ResponseEntity<Map<String, Object>> dislikePost(@PathVariable Long articleId) {
-
-        likeDislikeService.dislikeArticle(articleId);
-
-        // 좋아요 수 등 응답 데이터를 클라이언트로 보냄
-        Map<String, Object> response = new HashMap<>();
-        response.put("likeCount", likeDislikeService.likeCount(articleId));
-
-        response.put("dislikeCount", likeDislikeService.dislikeCount(articleId));
-        response.put("userAction", "dislike");
-
-        return ResponseEntity.ok(response);
-    }
 }
--- src/main/java/me/hyunggeun/springbootdeveloper/likedislike/service/LikeDislikeService.java
+++ src/main/java/me/hyunggeun/springbootdeveloper/likedislike/service/LikeDislikeService.java
@@ -6,6 +6,7 @@ import me.hyunggeun.springbootdeveloper.article.repository.ArticleRepository;
 import me.hyunggeun.springbootdeveloper.likedislike.dto.LikeDislikeResponseDTO;
 import me.hyunggeun.springbootdeveloper.likedislike.entity.LikeDislike;
 import me.hyunggeun.springbootdeveloper.likedislike.repository.LikeDislikeRepository;
+import me.hyunggeun.springbootdeveloper.security.CustomUserDetails;
 import me.hyunggeun.springbootdeveloper.user.entity.User;
 import me.hyunggeun.springbootdeveloper.user.repository.UserRepository;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -26,6 +27,62 @@ public class LikeDislikeService {
 
     private final ArticleRepository articleRepository;
 
+
+
+
+    public LikeDislikeResponseDTO likeDislikeArticle(Long articleId,String action){
+
+        CustomUserDetails principal = (CustomUserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String email = principal.getUsername();
+
+        Optional<Article> article = articleRepository.findById(articleId);
+
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        Optional<LikeDislike> likeDislike = likeDislikeRepository.findByUserIdAndArticleId(user.getId(), articleId);
+
+        if(likeDislike.isPresent()){
+
+            LikeDislike existingLikeDislike = likeDislike.get();
+
+            if (Boolean.TRUE.equals(existingLikeDislike.getLikeStatus()) && action.equals("like")) {
+                likeDislikeRepository.delete(existingLikeDislike);
+                action=null;
+
+            }else if (Boolean.FALSE.equals(existingLikeDislike.getLikeStatus()) && action.equals("like")) {
+
+                existingLikeDislike.changeLikeStatus(true); // dirty checking 자동 적용
+            }
+
+            else if (Boolean.FALSE.equals(existingLikeDislike.getLikeStatus()) && action.equals("dislike")) {
+                likeDislikeRepository.delete(existingLikeDislike);
+                action=null;
+
+            } else if (Boolean.TRUE.equals(existingLikeDislike.getLikeStatus()) && action.equals("dislike")) {
+                existingLikeDislike.changeLikeStatus(false);
+            }
+
+        }  else {
+            if (article.isPresent()) {
+                LikeDislike newLike = LikeDislike.create(user,article.get() , action.equals("like"));
+                likeDislikeRepository.save(newLike);
+            }
+            else{
+                throw new IllegalArgumentException("Article not found");
+            }
+
+        }
+
+
+        // 좋아요 수 등 응답 데이터를 클라이언트로 보냄
+
+        return  new LikeDislikeResponseDTO(action,likeCount(articleId), dislikeCount(articleId) );
+
+    }
+
     public int likeCount(Long postId){
         return likeDislikeRepository.likeCount(postId);
     }
@@ -34,78 +91,4 @@ public class LikeDislikeService {
         return likeDislikeRepository.dislikeCount(postId);
     }
 
-
-
-    public LikeDislikeResponseDTO likeArticle(Long articleId) {
-
-        // SecurityContextHolder에서 현재 사용자 정보 가져오기
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-
-        // username으로 User 조회
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
-        Optional<LikeDislike> likeDislike = likeDislikeRepository.findByUserIdAndArticleId(user.getId(), articleId);
-
-        if (likeDislike.isPresent()) {
-
-            LikeDislike existingLike = likeDislike.get();
-
-            if (Boolean.TRUE.equals(existingLike.getLikeStatus())) {
-                likeDislikeRepository.delete(existingLike);
-
-            } else {
-
-                existingLike.changeLikeStatus(true); // dirty checking 자동 적용
-            }
-        }
-        else {
-            Optional<Article> article = articleRepository.findById(articleId);
-            if (article.isPresent()) {
-                LikeDislike newLike = LikeDislike.create(user,article.get() ,true);
-                likeDislikeRepository.save(newLike);
-            }
-            else{
-                throw new IllegalArgumentException("Article not found");
-            }
-        }
-
-        // 좋아요 수 등 응답 데이터를 클라이언트로 보냄
-
-        LikeDislikeResponseDTO likeDislikeResponseDTO = new LikeDislikeResponseDTO("like",likeCount(articleId), dislikeCount(articleId) );
-
-
-        return likeDislikeResponseDTO;
-    }
-
-    public void dislikeArticle(Long articleId) {
-
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
-        Optional<LikeDislike> likeDislike = likeDislikeRepository.findByUserIdAndArticleId(user.getId(), articleId);
-
-        if (likeDislike.isPresent()) {
-            LikeDislike existingLike = likeDislike.get();
-            if (Boolean.FALSE.equals(existingLike.getLikeStatus())) {
-                likeDislikeRepository.delete(existingLike);
-            } else {
-                existingLike.changeLikeStatus(false);
-            }
-        } else {
-            LikeDislike newLike = LikeDislike.create(user, articleRepository.findById(articleId).get(), false);
-//            LikeDislike newLike = new LikeDislike();
-//            newLike.setUser(user);
-//            newLike.setPost(new Post(postId));
-//            newLike.setLikeStatus(true);
-            likeDislikeRepository.save(newLike);
-//            LikeDislike newLike = new LikeDislike();
-//            newLike.setUser(user);
-//            newLike.setPost(new Post(postId));
-//            newLike.setLikeStatus(false);
-//            likeDislikeRepository.save(newLike);
-        }
-    }
 }
